720d511d63accd47a510471ac7cee6cafd2ddb1d
ROBSON SILVA DE SOUSA <r.sousa.37@gmail.com>
2026-03-28 14:47:57 -0300

    Acréscimo do valor de Pi nas curiosidades

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const curiosidades = [
     "O número 0 foi inventado na Índia por volta do século V.",
     "O Teorema de Pitágoras era conhecido pelos babilônios 1000 anos antes de Pitágoras.",
     "A soma dos ângulos internos de um triângulo é sempre 180°.",
-    "O número Pi (π) é irracional, o que significa que suas casas decimais nunca terminam nem se repetem."
+    "O número π (Pi) é irracional, o que significa que suas casas decimais nunca terminam nem se repetem: 3,14159265358979323846..."
 ];
 function mostrarCuriosidade() {
     const elementoTexto = document.getElementById('texto-curiosidade');
