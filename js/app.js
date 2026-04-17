4bfd854b664452daa8b6ed8fa705a96780869dcb
ROBSON SILVA DE SOUSA <r.sousa.37@gmail.com>
2026-04-17 19:56:28 -0300

    Inclusão da calculadora (reeditado)

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 const { createApp } = Vue;
-
 createApp({
   data() {
     return {
       tituloGaleria: "Curiosidade do Dia",
-      // Deixe o campo atual vazio, pois o mounted vai preenchê-lo
       curiosidadeAtual: "", 
       curiosidades: [
         "O algarismo do número 0 foi inventado na Índia.",
         "A soma dos ângulos internos de um triângulo é igual 180°.",
         "O número Pi (π = 3,14159265358979323846…) tem infinitas casas decimais mas não é uma dízima periódica.",
-        "O matemática Euclides pode ter vivido em um período de até um século antes do aceito pela maioria dos estudiosos.",
-      ]
-    }
+        "O matemático Euclides pode ter vivido em um período de até um século antes do aceito pela maioria dos estudiosos.",
+      ],
+      // Dados da calculadora
+      base: 0,
+      altura: 0,
+      forma: 'retangulo'
+    };
   },
-  // O "segredo" está aqui: esta função roda assim que o site carrega
   mounted() {
     this.novaCuriosidade();
   },
@@ -22,6 +23,16 @@ createApp({
     novaCuriosidade() {
       const indice = Math.floor(Math.random() * this.curiosidades.length);
       this.curiosidadeAtual = this.curiosidades[indice];
+    }
+  },
+  computed: {
+    resultado() {
+      if (this.base <= 0 || this.altura <= 0) return null;
+      if (this.forma === 'triangulo') {
+        return (this.base * this.altura) / 2;
+      } else {
+        return this.base * this.altura;
+      }
     }
   }
 }).mount('#app');
