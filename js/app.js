e03a39037b76af4138580dbc007d1372da2fa13c
ROBSON SILVA DE SOUSA <r.sousa.37@gmail.com>
2026-05-02 12:21:39 -0300

    correção para lembrar do modo escuro e formatação correção da formatação de mensagens das curiosidades para o MathJax

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ createApp({
       curiosidadeAtual: "", 
       curiosidades: [
         "O algarismo do número 0 foi inventado na Índia.",
-        "A soma dos ângulos internos de um triângulo é igual 180°.",
-        "O número Pi (π = 3,14159265358979323846…) tem infinitas casas decimais mas não é uma dízima periódica.",
+        "A soma dos ângulos internos de um triângulo é igual $180°$.",
+        "O número Pi \( \pi \approx 3,14 \) tem infinitas casas decimais mas não é uma dízima periódica.",
         "O matemático Euclides pode ter vivido em um período de até um século antes do aceito pela maioria dos estudiosos.",
       ],
       base: 0,
@@ -18,14 +18,25 @@ createApp({
   },
   mounted() {
     this.novaCuriosidade();
+    // Verifica se existe algo salvo no 'theme'
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    this.darkMode = true;
+    document.body.classList.add('dark-mode');
+  } else {
+    this.darkMode = false;
+    document.body.classList.remove('dark-mode');
+  }
   },
   methods: { 
     toggleDarkMode() {
       this.darkMode = !this.darkMode;
       if (this.darkMode) {
-        document.body.classList.add('dark-mode');
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark'); // Salva a preferência
       } else {
         document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light'); // Salva a preferência
       }
     },
     novaCuriosidade() {
